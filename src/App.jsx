7f92450bd6fef10720bd48cd95ff19bc7e6a46b5
abuhamdan144-ship-it <abuhamdan144@gmail.com>
2026-08-26 01:54:52 +0400

    refactor: remove duplicate member card import
    Remove redundant import statement in App.jsx to clean up component initialization.

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import Elections from './components/Elections';
 import Membership from './components/Membership';
 import MemberCard from './components/MemberCard';
 import Admin from './components/Admin';
-import MemberCard from './components/MemberCard';
 import ScrollProgress from './components/ScrollProgress';
 import CursorTrail from './components/CursorTrail';
 import WhatsAppFloat from './components/WhatsAppFloat';
